a5a01ac70d8ab6a2b4b28c45ebe7ca8f2f1872b3
codekarimi <ckarimi676@gmail.com>
2025-06-02 19:59:21 +0300

    Navbar

--- converso/app/layout.tsx
+++ converso/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Navbar from "@/components/Navbar";
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -11,6 +12,9 @@ const bricolage = Bricolage_Grotesque({
 export const metadata: Metadata = {
   title: "Converso",
   description: "Real-time AI Teaching Platform",
+  icons: {
+    icon: "/images/logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -20,13 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${bricolage.variable} antialiased`}
-      >
-      <ClerkProvider appearance={{ variables: { colorPrimary: '#fe5933' }} }>
-      {children}
-      </ClerkProvider>
-       
+      <body className={`${bricolage.variable} antialiased`}>
+        <ClerkProvider appearance={{ variables: { colorPrimary: "#fe5933" } }}>
+          <Navbar />
+          {children}
+        </ClerkProvider>
       </body>
     </html>
   );
