3689c6b8ddf8bd7ecdaca2f59bf96036d5e6353e
codekarimi <ckarimi676@gmail.com>
2025-06-02 19:26:06 +0300

    Sentry Implementation

--- converso/app/page.tsx
+++ converso/app/page.tsx
@@ -1,8 +1,29 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionList from '@/components/CompanionList'
 import React from 'react'
+import {recentSessions} from "@/constants";
+import CTA from '@/components/CTA';
 
 const page = () => {
   return (
-    <div>page</div>
+    <main>
+      <h1>Popular Companions </h1>
+
+      <section className="home-section">
+        <CompanionCard/>
+
+      </section>
+      <section className="home-section">
+
+      <CompanionList
+                title="Recently completed sessions"
+                companions={recentSessions}
+                classNames="w-2/3 max-lg:w-full"
+            />
+            <CTA/>
+
+      </section>
+    </main>
   )
 }
 
---
+++ converso/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://1169798386ce1b08aaf3f9503268b958@o4508810874716160.ingest.de.sentry.io/4509429975547984",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
