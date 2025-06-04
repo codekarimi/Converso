da0b3e8ef5a042b256b8d89cc0d6c231b44bce67
codekarimi <ckarimi676@gmail.com>
2025-06-04 14:43:50 +0300

    companion form

--- converso/app/page.tsx
+++ converso/app/page.tsx
@@ -3,15 +3,23 @@ import CompanionList from '@/components/CompanionList'
 import React from 'react'
 import {recentSessions} from "@/constants";
 import CTA from '@/components/CTA';
+import { getSubjectColor } from '@/lib/utils';
 
 const page = () => {
+
+  const companions= recentSessions;
   return (
     <main>
       <h1>Popular Companions </h1>
 
       <section className="home-section">
-        <CompanionCard/>
-
+      {companions.map((companion) => (
+                <CompanionCard
+                    key={companion.id}
+                    {...companion}
+                    color={getSubjectColor(companion.subject)}
+                />
+            ))}
       </section>
       <section className="home-section">
 
@@ -20,6 +28,7 @@ const page = () => {
                 companions={recentSessions}
                 classNames="w-2/3 max-lg:w-full"
             />
+            
             <CTA/>
 
       </section>
