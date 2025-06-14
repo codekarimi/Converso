adb9f44ff8c17669469b945611fd88e0a39f0486
codekarimi <ckarimi676@gmail.com>
2025-06-14 21:36:49 +0300

    subscription Management

--- converso/app/companions/new/page.tsx
+++ converso/app/companions/new/page.tsx
@@ -9,11 +9,11 @@ const NewCompanion = async () => {
     const { userId } = await auth();
     if(!userId) redirect('/sign-in');
 
-    // const canCreateCompanion = await newCompanionPermissions();
+    const canCreateCompanion = await newCompanionPermissions();
 
     return (
         <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
-            {/* {canCreateCompanion ? ( */}
+            {canCreateCompanion ? (
 
                 <article className="w-full gap-4 flex flex-col">
                     <h1>Companion Builder</h1>
@@ -21,7 +21,7 @@ const NewCompanion = async () => {
                     <CompanionForm />
                 </article>
                 
-             {/* ) : (
+             ) : (
                  <article className="companion-limit">
                      <Image src="/images/limit.svg" alt="Companion limit reached" width={360} height={230} />
                      <div className="cta-badge">
@@ -33,7 +33,7 @@ const NewCompanion = async () => {
                              Upgrade My Plan
                          </Link>
                      </article>
-                 )} */}
+                 )}
         </main>
     )
 }
